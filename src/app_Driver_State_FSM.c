76136cc31e3614de6ae85b0ad946fdbb5798495d
jztony <h1114951783@outlook.com>
2019-12-04 15:51:26 -0500

    update changes we made in the past months

--- src/app_Driver_State_FSM.c
+++ src/app_Driver_State_FSM.c
@@ -19,9 +19,10 @@ DRIVE_RTN: error
     app_driver_state_fsm.c
 Forgive me if the comment is not clear, because the guy who originally wrote this code DID NOT HAVE ANY COMMENT.
 If you are seeing this, hope you can comment your code in any of your projects. 
-*/
+	
   Summary:
     This file contains the source code for the MPLAB Harmony application.
+
   Description:
     This file contains the source code for the MPLAB Harmony application.  It 
     implements the logic of the application's state machine and it may call 
@@ -36,12 +37,15 @@ If you are seeing this, hope you can comment your code in any of your projects.
 // DOM-IGNORE-BEGIN
 /*******************************************************************************
 Copyright (c) 2013-2014 released Microchip Technology Inc.  All rights reserved.
+
 Microchip licenses to you the right to use, modify, copy and distribute
 Software only when embedded on a Microchip microcontroller or digital signal
 controller that is integrated into your product or third party product
 (pursuant to the sublicense terms in the accompanying license agreement).
+
 You should refer to the license agreement accompanying this Software for
 additional information regarding your rights and obligations.
+
 SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
 EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
 MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
@@ -64,130 +68,535 @@ SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
 
 #include "app_driver_state_fsm.h"
 
-@@ -24,6 +63,7 @@ uint32_t buzz_timer;
+APP_DRIVER_STATE_FSM_DATA app_driver_state_fsmData;
+uint32_t reading_mc_11; // MC+
+uint32_t reading_mc_22; // TSV
+uint32_t send_cond_timer; // timer for sending conditions
+uint32_t buzz_timer;
 uint32_t buzz_sound_count;
+uint32_t heartbeat_timer;
+uint32_t red_timer;
+uint32_t blue_timer;
+uint8_t state_led;
 int reading_1;
 int reading_2;
 
+// high current variables
 uint32_t cur_sen_cntr;
 uint32_t cur_sen_val;
 uint32_t cur_nor_val;
-@@ -40,6 +80,9 @@ void APP_DRIVER_STATE_FSM_Initialize ( void )
+uint32_t cur_hv_val;
+
+// moving-averaged value
+uint32_t mc_voltage_val;
+uint32_t ts_voltage_val;
+
+// pwm counter
+uint32_t pwm_cntr;
+
+// array to store moving average history
+uint32_t hv_avg[mov_avg_size];
+uint32_t mc_avg[mov_avg_size];
+uint32_t ts_avg[mov_avg_size];
+
+// D_LED_CTRL flash counter
+uint32_t flash_cntr;
+
+// initialize return condition enum
+return_condition rc = (return_condition)0;
+
+void APP_DRIVER_STATE_FSM_Initialize ( void )
+{
+    /* Place the App state machine in its initial state. */
+    app_driver_state_fsmData.state = APP_DRIVER_STATE_FSM_STATE_INIT;
+    send_cond_timer = 0;
+    buzz_timer = 0;
+    buzz_sound_count = 0;
+    DriveSetUpDriveBtnRelease = 0;
+    
     cur_sen_cntr = 0;
     cur_sen_val = 0;
     cur_nor_val = 0;
+    cur_hv_val = 0;
+    flash_cntr = 0;
+    
+    mc_voltage_val = 0;
+    ts_voltage_val = 0;
+    state_led = 0b000;
     /* TODO: Initialize your application's state machine and other
      * parameters.
      */
 }
 
 
-@@ -207,6 +250,10 @@ void APP_DRIVER_STATE_FSM_Tasks ( void )
+void APP_DRIVER_STATE_FSM_Tasks ( void )
+{
+    if (send_condition_flag) {
+        send_condition_flag = 0;
+        send_cond_timer++;
+        update_value();
+//        buzz_timer++;
+        if (send_cond_timer == 10) {
+            send_conditions();
+            send_cond_timer = 0;
+//            PORTBbits.RB2 = ~PORTBbits.RB2;
+        }
+//        if (buzz_timer > 10) {
+////            uint8_t a = PORTBbits.RB15;
+////            PORTBbits.RB15 = ~PORTBbits.RB15;
+//            buzz_timer = 0;
+//            if (app_driver_state_fsmData.state == DRIVE_RTDS && buzz_sound_count < 6) {
+//                buzz_sound_count++;
+//                PORTBbits.RB15 = ~PORTBbits.RB15;
+//            }
+//        }
+        
+    }
+    
+    // LED lights up according to state_led bits
+//    PORTBbits.RB2 = ((0b00000100 & state_led) == 0b00000100);
+//    PORTBbits.RB3 = ((0b00000010 & state_led) == 0b00000010);
+//    PORTBbits.RB4 = ((0b00000001 & state_led) == 0b00000001);
+//      PORTBbits.RB2 = 1;
+//      PORTBbits.RB3 = 1;
+//      PORTBbits.RB4 = 1;
+    
+//    PORTBbits.RB2 = saftyloop;
+    
+      PORTBbits.RB2 = drive_btn_pushing;
+      PORTBbits.RB3 = throttlepressed;
+      PORTBbits.RB4 = (throttle < 0xF00);
+    
+    
+    /* Check the application's current state. */
+    switch ( app_driver_state_fsmData.state )
+    {
+        /* Application's initial state. */
+        case APP_DRIVER_STATE_FSM_STATE_INIT:
+        {
+            states = 0x0;
+            bool appInitialized = true;
+//            BUZZER_CTRL=0;
+            D_LED_CTRL=0;
+            Throttle_SEL=0;
+            Cooling_Relay_CTRL=0;
+            Spare_LED_CTRL = 0;
+            // initialize return condition to 0
+            rc = drive_ok;
+            // state led
+            state_led = 0b000;
+            
+            if (appInitialized)
+            {            
+                app_driver_state_fsmData.state = IDLE;
             }
             break;
         }
 
-
+        case IDLE:
+        {
+            states = 0x1;
+//            BUZZER_CTRL=0;
+            D_LED_CTRL=0;
+            Throttle_SEL=1;
+            Cooling_Relay_CTRL=0;
+            Spare_LED_CTRL = 0;
+            
+            state_led = 0b001;
+            
+            if (rc == safety_loop) {
+                if (flash_cntr < 800000000) {
+                    flash_cntr++;
+                } else {
+                    flash_cntr = 0;
+                }
+                D_LED_CTRL = (flash_cntr < 400000000);
+            }
+            
+            if(saftyloop){
+                app_driver_state_fsmData.state = PRECHARGE;
+            }
+            break;
+        }
+        
+        case PRECHARGE:
+        {
+            states = 0x2;
+//            BUZZER_CTRL=0;
+            D_LED_CTRL=0;
+            rc = drive_ok;
+            Throttle_SEL=0;
+            Cooling_Relay_CTRL=0;
+            Spare_LED_CTRL = 0;
+            
+            state_led = 0b010;
+            
+            if(saftyloop == 0){
+                rc = safety_loop;
+                app_driver_state_fsmData.state = IDLE;
+            } else if(PC_Ready){
+                app_driver_state_fsmData.state = DRIVE_SETUP;
+            }
+            break;
+        }
+        
+        case DRIVE_SETUP: 
+        {
+            update_value();
+            states = 0x3;
+            Throttle_SEL=0;
+            Cooling_Relay_CTRL=1;
+//            D_LED_CTRL = 0;
+            Spare_LED_CTRL = 0;
+//            buzz_sound_count = 0; // reset buzzer sound time counter
+            
+            state_led = 0b011;
+            
+            // D_LED blink differently for different return conditions
+            if (rc == throttle_Implausible) {
+                if (flash_cntr < 400000000) {
+                    flash_cntr++;
+                } else {
+                    flash_cntr = 0;
+                }
+                D_LED_CTRL = (flash_cntr < 100000000);
+            } else if (rc == throttle_brake) {
+                if (flash_cntr < 600000000) {
+                    flash_cntr++;
+                } else {
+                    flash_cntr = 0;
+                }
+                D_LED_CTRL = (flash_cntr < 100000000 | 
+                (flash_cntr < 300000000 & flash_cntr > 200000000));
+            } else if (rc == mc_not_active) {
+                if (flash_cntr < 40000000) {
+                    flash_cntr++;
+                } else {
+                    flash_cntr = 0;
+                }
+                D_LED_CTRL = (flash_cntr < 20000000);
+            }else if (rc == drive_ok) {
+                D_LED_CTRL = 0;
+            }
+            
+            if(saftyloop == 0){
+                rc = safety_loop;
+                app_driver_state_fsmData.state = IDLE;
+            } else if(drive_btn_pushing & !throttleImplausibility & brakePressed & MC_Activate & !throttlepressed){
+                app_driver_state_fsmData.state = DRIVE_RTDS;
+                // and throttle not pressed
+            }
+            
+            break;
+        }
+        
+        
+        // use the opamp to do calculation
+        // add throttle voltage to be sent
+        // send two packet 
+        // mc voltage
+        // add spare bits in can message
+        case DRIVE_RTDS:
+        {
+            states = 0x4;
+            PORTEbits.RE0 = 1;
+            // clear return condition
+            rc == drive_ok;
+            flash_cntr = 0;
+            if (buzz_timer_flag) {
+                buzz_timer_flag = 0;
+                buzz_timer++;
+                if (buzz_timer > 10) {
+                    buzz_timer = 0;
+                    if (buzz_sound_count < 6) {
+                        buzz_sound_count++;
+                        PORTEbits.RE0 = 1;
+                    }
+                    //add another state count to 10
+                }
+            }
+            
+            state_led = 0b100;
+            
+            if(saftyloop == 0){
+                PORTEbits.RE0 = 0;
+                rc = safety_loop;
+                app_driver_state_fsmData.state = IDLE;
+            }
+            else if (buzz_sound_count >= 6) {
+                PORTEbits.RE0 = 0;
+                if (!drive_btn_pushing && brakePressed && !throttlepressed) {
+                    app_driver_state_fsmData.state = DRIVE;
+                    // if throttle pressed and brake can't go
+                }
+            }
+            break;
+        }
+        
+        case DRIVE:
+        {
+            //buzz_sound_count = 0;
+            // set ctrl to 0
+            states = 0x5;
+            D_LED_CTRL=1;
+            Throttle_SEL=1;
+//            Cooling_Relay_CTRL=1;
+//            BUZZER_CTRL=~buzz_flag;
+            Spare_LED_CTRL = 1;
+            // no need to clear count
+            
+            state_led = 0b101;
+            
+            if(saftyloop == 0){
+                rc = safety_loop;
+                app_driver_state_fsmData.state = IDLE;
+            } else if(throttleImplausibility | (throttlepressed & brakePressed) | !MC_Activate | (drive_btn_pushing & brakePressed)){
+                // different blinking for different return condition
+                if (throttleImplausibility) {
+                    rc = throttle_Implausible;
+                } else if (throttlepressed & brakePressed) {
+                    rc = throttle_brake;
+                } else if (!MC_Activate) {
+                    rc = mc_not_active;
+                } else if (drive_btn_pushing & brakePressed) {
+                    rc = drivebtn_brake;
+                }              
+                
+                app_driver_state_fsmData.state = DRIVE_RETURN;
+            } else if(overCurr){
+                rc = over_current_cond;
+                app_driver_state_fsmData.state = OVERCURRENT;
+            }
+            
+            break;
+        }
+        
+        case OVERCURRENT:
+        {
+            states = 0x6;
+            //D_LED_CTRL flash
+//            BUZZER_CTRL=0;
+//            D_LED_CTRL=1;
+            Throttle_SEL=0;
+            Cooling_Relay_CTRL=1;
+            Spare_LED_CTRL = 0;
+            // blink D_LED to indicate state
+            if (flash_cntr < 200000000) {
+                flash_cntr++;
+            } else {
+                flash_cntr = 0;
+            }
+            D_LED_CTRL = (flash_cntr < 100000000);
+            
+            state_led = 0b110;
+            
+            if(saftyloop == 0){
+                rc = safety_loop;
+                app_driver_state_fsmData.state = IDLE;
+            } else if(!overCurr & throttlegreaterthan){
+                app_driver_state_fsmData.state = DRIVE;
+            } else {
+                // use 50% pwm to reduce throttle
+                if (pwm_cntr < 10000) {
+                    pwm_cntr++;
+                } else {
+                    pwm_cntr = 0;
+                }
+                Throttle_SEL = (pwm_cntr < 5000);
+            }
+            break;
+        }
+        
+        case DRIVE_RETURN: 
+        {
+            // turn off drive LED
+            // different frequency to blink for different problem
+            states = 0x8;
+            
+            state_led = 0b111;
+            
+            // flash to tell the driver to stop pushing drive button
+            if (flash_cntr < 1000000000) {
+                flash_cntr++;
+            } else {
+                flash_cntr = 0;
+            }
+            D_LED_CTRL = (flash_cntr < 100000000 | 
+            flash_cntr > 200000000 & flash_cntr < 300000000 |
+            flash_cntr > 400000000 & flash_cntr < 500000000);
+            
+            if(saftyloop == 0){
+                rc = safety_loop;
+                app_driver_state_fsmData.state = IDLE;
+            }
+            if (!drive_btn_pushing) {
+                app_driver_state_fsmData.state = DRIVE_SETUP;
+            }
+            
+            break;
+        }
+        
+        
         /* TODO: implement your application state machine.*/
-
+        
 
         /* The default state should never be executed. */
         default:
-@@ -219,60 +266,54 @@ void APP_DRIVER_STATE_FSM_Tasks ( void )
+        {
+            /* TODO: Handle error in application's state machine. */
+            break;
+        }
+    }
+}
 
 
 void update_value() {
     // read values from I/O Expander
     // see ThrottlePlausibility page in schematic
     int MCP23016B_reading = get_MCP23016B();
-
-    // pin48
+    
+    
 //    PC_Ready = ((MCP23016B_reading & 0x80) == 0x80); 
 //    PC_Ready = (PORTDbits.RD9 != 0);
 //    PC_Ready = !PORTDbits.RD9;
+    // pin48
     PC_Ready = !PORTCbits.RC14;
 //    PC_Ready = 1;
-
-    // GPB6
+    
 //    throttleImplausibility = !((MCP23016B_reading & 0x3F) == 0x3F);
+    // GPB6
     throttleImplausibility = ((MCP23016B_reading & 0b01000000) == 0b01000000);
-
+    
     // updated from CANBus
     overCurr = over_current();
-
-    // name should be safetyLoop?
+    
+    // name should be safetyloop
     // pin4
     saftyloop = !PORTGbits.RG6;
 //    saftyloop = get_ADCCh(45)/0x10;
-
+    
 //    DriverButton_Pushed = PORTDbits.RD1;
-
-    // pin 64
+    // pin64
     brakePressed = !PORTEbits.RE4;
-
     // when MC+ > TSV - 10
     MC_Activate = mc_active();
 //    MC_Activate = 1;
-
-
+    
     // read values from an ADC on board
     // see isolators page in schematic
     reading_mc_11 = get_NCD9830_reading(7);
     reading_mc_22 = get_NCD9830_reading(6);
-    a1 = get_NCD9830_reading(3);
-    a2 = get_NCD9830_reading(4);
-
+    
     // TODO: input of get_ADCCh should be analog input port# (e.g. AN1)
     PORTDbits.RD0 = D_LED_CTRL;
     PORTBbits.RB8 = Throttle_SEL;
 
     PORTDbits.RD11 = Spare_LED_CTRL;
+    
+    throttle = get_ADCCh(0);
 
-    throttle = get_ADCCh(0)/0x10;
-
-    FlowRate = flow_rate_freq;
+    //FlowRate = flow_rate_freq;
 //    throttle = get_ADCCh(14)/0x10;
-
+    
     throttlegreaterthan = throttle > 0x26; // 0.8v
-    throttlepressed = throttle > 0x50; // 1.2v
-
+    throttlepressed = throttle > 0b010000000000; // 1.2v
+    
     // updates by interrupt at RPD1 (FlowRate input to PIC32)
     // to change the interrupt input port
-    // go to system_config/default/framework/system/ports/src/sys_ports.static.c 
-    FlowRate = flow_rate_freq;
+    // go to system_config/default/framework/system/ports/src/sys_ports.static.c
     a1 = get_NCD9830_reading(3);
     a2 = get_NCD9830_reading(4);
-
+    
     cur_sen_cntr++;
     cur_sen_val = cur_sen_val + abs(a2-a1);
+//    cur_hv_val = 1000 * cur_sen_val/cur_sen_cntr;
+//    cur_hv_val = moving_avg(hv_avg, a1-a2);
+    cur_hv_val = abs(a1 - a2);
+
     cur_nor_val = cur_sen_val/cur_sen_cntr;
-
+    
     // should not work. see throttle
-    CoolTemp_1 = get_ADCCh(1)/0x10;
-    CoolTemp_2 = get_ADCCh(2)/0x10;
-
-    PORTDbits.RD0 = D_LED_CTRL;
-    PORTBbits.RB8 = Throttle_SEL;
-    PORTDbits.RD11 = Spare_LED_CTRL;
+    CoolTemp_1 = get_ADCCh(1)/16;
+    CoolTemp_2 = get_ADCCh(2)/16;
+//    CoolTemp_1 = get_ADCCh(5);
+//    CoolTemp_2 = get_ADCCh(7);
     // for testing
 //    PORTBbits.RB3 = DriverButton_Pushed;
 }
 
 int over_current() {
-@@ -300,7 +341,11 @@ void send_conditions() {
+    return cur_nor_val >= overCurr_val;
+}
+
+int mc_active() {
+    
+    reading_mc_11 = get_NCD9830_reading(7); // MC+
+    reading_mc_22 = get_NCD9830_reading(6); // TSV
+	mc_voltage_val = moving_avg(mc_avg, reading_mc_11);
+	ts_voltage_val = moving_avg(ts_avg, reading_mc_22);
+	
+    
+/*     if (mc_voltage_sum + reading_mc_11 > mc_voltage_sum) {
+//    if (mc_voltage_cntr <= 100) {
+        mc_voltage_cntr++;
+        mc_voltage_sum += reading_mc_11;
+        mc_voltage_val = mc_voltage_sum / mc_voltage_cntr;
+    } 
+//    else {
+//        mc_voltage_cntr = 1;
+//        mc_voltage_sum = reading_mc_11;
+//    }
+    
+    
+    if (ts_voltage_sum + reading_mc_22 > ts_voltage_sum) {
+//    if (ts_voltage_cntr <= 100) {
+        ts_voltage_cntr++;
+        ts_voltage_sum += reading_mc_22;
+        ts_voltage_val = ts_voltage_sum / ts_voltage_cntr;
+    } 
+//    else {
+//        ts_voltage_cntr = 1;
+//        ts_voltage_sum = reading_mc_22;
+//    } */
+    
+    
+//    if (reading_mc_11 > reading_mc_22) return 1;
+//    if (reading_mc_11 > 0) return 1;
+    // mc_22 - 10
+//    else return 0;
+    return 1;
+}
+
+int get_pad_voltage() {
+    return get_ADCCh(8);
+    
+}
+
+int return_Throttp() {
+    return (int)throttleImplausibility;
+}
+
+void send_conditions() {
     uint8_t conditions = throttlepressed + (throttleImplausibility*2) + (overCurr*4) + (saftyloop*8) + (drive_btn_pushing*16) + 
                          (brakePressed*32) + (MC_Activate*64) + (PC_Ready*128);
-
-    can_send_bytes(0x555,conditions,reading_mc_11,reading_mc_22,CoolTemp_1,CoolTemp_2,flow_rate_freq,states,cur_nor_val);
-
-    can_send_bytes(0x555,conditions,reading_mc_11,reading_mc_22,CoolTemp_1,CoolTemp_2,flow_rate_freq,states,cur_nor_val);
+    
+    can_send_bytes(0x555,conditions,mc_voltage_val,ts_voltage_val,CoolTemp_1,CoolTemp_2,flow_rate_raw,states,cur_hv_val);
+    can_send_bytes(0x596,0x0A,throttle,0x00,0x00,0x00,0x00,0x00,0x00);
+    // send one more frame to a different address, check space availability
     cur_sen_cntr = 0;
     cur_sen_val = 0;
     
-@@ -311,3 +356,6 @@ void send_conditions() {
+    //mc_voltage_val = 0;
+    //ts_voltage_val = 0;
+    
+//    boardtemp = get_ADCCh(9);
+//    can_send_bytes(0x300,get_ADCCh(0)/0x10,get_ADCCh(1)/0x10,get_ADCCh(2)/0x10,get_ADCCh(4)/0x10,3,get_ADCCh(9),PC_Ready,rand() % 0xFF);
+//    can_send_bytes(0x300,a2,a1,abs(a2),abs(a1),3,3,4,4);
+//    can_send_bytes(0x300,states,reading_1,reading_2,reading_mc_11,reading_mc_22,a1,a2,abs(a2-a1));
 //    rand() % 0xFF
 //    can_send_bytes(0x400,a1,a2,reading_mc_22,CoolTemp_1,CoolTemp_2,FlowRate,states,rand() % 0xFF);
 }
+
 /*******************************************************************************
  End of File
  */
--- src/app_Driver_State_FSM.h
+++ src/app_Driver_State_FSM.h
@@ -3,6 +3,8 @@
 #ifndef _APP_DRIVER_STATE_FSM_H
 #define _APP_DRIVER_STATE_FSM_H
 
+#define mov_avg_size 10
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: Included Files
@@ -57,13 +59,26 @@ typedef struct
 //    uint8_t throttleControl;
 //} driver_state_fsm_conditions;
 
+typedef enum {
+    // different cases that car exits drive state
+    drive_ok = 0,
+    safety_loop,
+    over_current_cond,
+    drivebtn_brake,
+    throttle_Implausible,
+    throttle_brake,
+    mc_not_active
+            
+} return_condition;
+
 void APP_DRIVER_STATE_FSM_Initialize ( void );
 
 void APP_DRIVER_STATE_FSM_Tasks( void );
 
 int saftyLoopClosed,PC_Ready, DriverButton_Pushed, throttleImplausibility, brakePressed, overCurr, airsOpen, MC_Activate, saftyloop, throttlegreaterthan, throttlepressed;
 int BUZZER_CTRL, D_LED_CTRL, Throttle_SEL, Cooling_Relay_CTRL, Spare_LED_CTRL;
-int throttle, boardtemp,CoolTemp_1,CoolTemp_2,FlowRate,a1,a2;
+uint32_t throttle;
+int boardtemp,CoolTemp_1,CoolTemp_2,FlowRate,a1,a2;
 void update_value();
 int states;
 int buzz_flag;
@@ -71,6 +86,44 @@ volatile uint32_t send_condition_flag; // triggered by interrupt when timer coun
 volatile uint32_t buzz_timer_flag;
 volatile uint32_t flow_rate;
 int DriveSetUpDriveBtnRelease;
+
+// function declaration
+uint32_t moving_avg(uint32_t *last_vals, uint32_t update) {
+	int full = 1;
+	uint32_t sum = 0;
+	uint32_t mov_avg = 0;
+	int i = 0;
+    // has to hard code this because variable array size not supported by MPLAB complier (XC32, which is not even C99)
+    uint32_t data_size = mov_avg_size;
+	
+//	while (i < data_size) {
+//		if (*(last_vals + i) == 0) {
+//			*(last_vals + i) = update;
+//			sum += *(last_vals + i);
+//			full = 0;
+//			mov_avg = sum / (i+1);
+//			break;
+//		} else {
+//			sum += *(last_vals + i);
+//		}
+//		i++;
+//	}
+//	
+//	if (full) {
+		sum = 0;
+		i = 0;
+		while (i < data_size - 1) {
+			*(last_vals + i) = *(last_vals + i + 1);
+			sum += *(last_vals + i);
+			i++;
+		}
+		*(last_vals + data_size - 1) = update; 
+		sum += *(last_vals + data_size - 1);
+		mov_avg = sum / data_size;
+//	}
+	
+	return mov_avg;
+}
 
 
 #endif /* _APP_DRIVER_STATE_FSM_H */
